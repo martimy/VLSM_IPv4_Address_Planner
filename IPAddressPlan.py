0c66ce52c698f77e145105742e5db6ce4e99a897
Maen Artimy <artimym@gmail.com>
2021-09-25 13:58:52 -0300

    adding commnets and changing variable names

--- IPAddressPlan.py
+++ IPAddressPlan.py
@@ -1,69 +1,80 @@
 import math
 from binpackmod import BinPack
-
+import ipaddress
 
 class AddressPlan4(object):
 
     def run(self, network, org, growth=1.0):
-        labels = list(org.keys())
-        req = list(org.values())
+        # Sort the requirements
+        s = {k: v for k, v in sorted(org.items(), key=lambda item: item[1], reverse=True)}
+        labels = list(s.keys())
+        req = list(s.values())
+        
         # Get the number of bits needed to fulfill the requirements
         bits = [math.ceil(math.log(r * growth + 2, 2)) for r in req]
 
-        bin_value, mask = self.__addr_to_dec(network)
-        # return a list of the mask extensions needed to split the network address, and
-        # the number of address blocks available for each mask length
-        ext, blocks, utilization = self.__check(bits, mask)
-        if ext is None:
+        # Get the int value of the address and its mask length
+        x = ipaddress.ip_network(network)
+        int_value, mask = int(x.network_address), x.prefixlen
+                
+        # return a list of the mask extensions needed to split the network 
+        # address, and the number of address blocks available for each mask 
+        # length
+        ext_bits, num_subnets, utilization = self.__get_subnets(bits, mask)
+        if (min(ext_bits) < 0) or (utilization > 1):
             raise ValueError('Network mask is too short!')
 
         # Ratio of blocks relative to the smallest block
         # these are the BinPack requirements
-        u = [max(blocks) // i for i in blocks]
-        u_size = 2 ** min(bits)
+        self.blocks = [max(num_subnets) // i for i in num_subnets]
+        num_bins = min(num_subnets)
+        bin_size = max(self.blocks)
+        
+        bins = BinPack(num_bins, bin_size)
+        fitted = bins.fit(self.blocks)
+        
+        # maps the positions in bins to offset numbers
+        result = [k[0] * bin_size + k[1] for k in fitted]
+        self.total = 2 ** (32-mask)
 
-        bins = BinPack(min(blocks), max(u))  # Num. of bins and bin size (min c and max u)
-        result, _ = bins.fit(u)
+        # convert the position offests to bits offsets
+        self.block_size = 2 ** min(bits)      
+        self.mult = [i * self.block_size for i in result]
 
-        mult = [i * u_size for i in result]
-        assgn = [self.__addr_to_str(bin_value + k[0], mask + k[1]) for k in zip(mult, ext)]
-        return zip(labels, req, [2 ** b - 2 for b in bits], assgn), utilization
+        assgn = [self.__addr_to_str(int_value + k[0], mask + k[1]) for k in zip(self.mult, ext_bits)]
+        return zip(labels, req, [2 ** b - 2 for b in bits], assgn, self.mult), utilization
 
-
-    def __addr_to_dec(self, netstr):
-        # get the network address as a list of four decimal values
-        addrlist, mask = self.__addr_to_list(netstr)
-        dec = int(sum([256 ** (3 - y) * addrlist[y] for y in range(4)]))
-        return dec, mask
-
-    # Take a string representation of  network address and return
-    # list of four decimals and a mask
-    def __addr_to_list(self, net):
-        addr = net.split('/')
-        return [int(x) for x in addr[0].split('.')], int(addr[1])
-
+    
     def __addr_to_str(self, b, m):
-        lst0 = b & 255
-        lst1 = b >> 8 & 255
-        lst2 = b >> 16 & 255
-        lst3 = b >> 24 & 255
-        addr_str = '.'.join(str(i) for i in [lst3, lst2, lst1, lst0])
-        addr_str += '/' + str(m)
-        return addr_str
+        return f"{ipaddress.IPv4Address(b).exploded}/{m}"
 
-    def __check(self, bits, mask):
-        '''
-           Check if the biggest subnet can be accommodated
-           Check if the total subnet sizes can be accommodated
-        '''
-        ext = [32 - mask - m for m in bits]
-        blocks = [2 ** i for i in ext]
-        total = sum([1 / i for i in blocks])
-        if (min(ext) < 0) or (total > 1):
-            return None, None, None
+    def __get_subnets(self, bits, mask_len):
+        """
+        get the number of bit needed for subnetting and the number of 
+        resultant subnets.
+        """
+        
+        ext_bits = [32 - mask_len - n for n in bits]
+        num_subnets = [2 ** n for n in ext_bits]
+        total = sum([1 / n for n in num_subnets])
 
-        return ext, blocks, total
+        return ext_bits, num_subnets, total
 
+    def get_map(self):
+        offset = [i//self.block_size for i in self.mult]
+        print("Total:", self.total, ", Blocks:", self.blocks)        
+        print("Block size:", self.block_size, ", Offset:", offset)
+        
+        num_blocks = self.total // self.block_size
+        tiles = "-" * num_blocks
+        
+        count = len(offset)
+        for i in range(count):
+            s_str = tiles[0:offset[i]] + "#" * self.blocks[i]
+            tiles =  s_str + tiles[len(s_str):]
+        print(tiles)
+        
+        
     @staticmethod
     def display(result):
         print("=" * 52)
@@ -78,11 +89,11 @@ if __name__ == '__main__':
     network = '192.168.64.0/19'
     req_subnets = {'A': 1777, 'B': 1560, 'C': 500, 'D': 672, 'E': 123, 'F': 904, 'G': 677, 'H': 67}
     
-
     plan = AddressPlan4()
     result, alloc = plan.run(network, req_subnets)
     AddressPlan4.display(result)
-
+    plan.get_map()
+    
     network = '10.10.0.0/21'
     req_subnets = {'A': 177, 'B': 160, 'C': 50, 'D': 62, 'E': 123, 'F': 104, 'G': 67, 'H': 67}
         
@@ -90,8 +101,9 @@ if __name__ == '__main__':
     #plan.run_old(network, req_subnets)
     result, alloc = plan.run(network, req_subnets)
     AddressPlan4.display(result)
-
-    network = '192.168.16.0/19'
+    plan.get_map()
+    
+    network = '192.168.32.0/19'
     req_subnets = {'Operations1': 2150, 'Operations2': 975, 'Operations3': 175, 'Sales': 575, 'DMZ': 5}
 
     plan = AddressPlan4()
@@ -99,7 +111,8 @@ if __name__ == '__main__':
     result, alloc = plan.run(network, req_subnets, growth=1.0)
     AddressPlan4.display(result)
     print('Allocated = {:0.1f}'.format(alloc))
-
+    plan.get_map()
+    
     network = '192.168.0.0/18'
     req_subnets = {'B1_Legal': 120, 'B1_Acc': 370, 'B1_DMZ': 5, 'B2_HQ': 1580, 'B2_Eng': 200, 'B2_DMZ': 5,
                    'B3_Operations1': 2150, 'B3_Operations2': 975, 'B3_Operations3': 175, 'B3_Sales': 575, 'B3_DMZ': 5,
@@ -109,3 +122,14 @@ if __name__ == '__main__':
     result, alloc = plan.run(network, req_subnets, growth=1.0)
     AddressPlan4.display(result)
     print('Allocated = {:0.1f}'.format(alloc))
+    plan.get_map()
+    
+    network = '192.168.4.0/24'
+    req_subnets = {'A': 72, 'B': 15, 'C': 15, 'D': 6}
+
+    plan = AddressPlan4()
+    result, alloc = plan.run(network, req_subnets, growth=1.5)
+    AddressPlan4.display(result)
+    print('Allocated = {:0.1f}'.format(alloc))
+    plan.get_map()
+    
--- binpackmod.py
+++ binpackmod.py
@@ -5,9 +5,10 @@ class BinPack(object):
         self.BIN_SIZE = bin_size
 
 
-    def fit(self, req, type="FIRST"):
+    def fit_old(self, req, type="FIRST"):
         # sort the requirements in decreasing order and return the index
         sorted_idx = sorted(range(len(req)), key=lambda k: req[k], reverse=True)
+        print(sorted_idx)
         self.slack = [self.BIN_SIZE] * self.BINS
 
         if type == "BEST":
@@ -17,27 +18,48 @@ class BinPack(object):
         else: # FIRST
             fitted = [self.__find_first_bin(req[j]) for j in sorted_idx]
 
-        if not (None in fitted):
+        if None not in fitted:
             original_order = [0] * len(sorted_idx)
             for i in range(len(sorted_idx)):
                 original_order[sorted_idx[i]] = fitted[i]
             return [k[0] * self.BIN_SIZE + k[1] for k in original_order], original_order
         return None
+    
+    def fit(self, req, type="FIRST"):
+        self.slack = [self.BIN_SIZE] * self.BINS
+
+        if type == "BEST":
+            fitted = [self.__find_best_bin(r) for r in req]
+        elif type == "WORST":
+            fitted = [self.__find_worst_bin(r) for r in req]
+        else: # FIRST
+            fitted = [self.__find_first_bin(r) for r in req]
+
+        return fitted
 
     def __find_first_bin(self, req):
+        # Search bins until one that can accomodate the requirements is found.
+        
         b = 0
         while b < self.BINS and req > self.slack[b]:
             b += 1
     
+        # Return if no bin is found
         if b == self.BINS:
             return None
     
+        # Update the remaining space
         c = self.BIN_SIZE - self.slack[b]
         self.slack[b] -= req
+        
+        # return the bin number and the starting position of space used
         return b, c
     
     def __find_best_bin(self, req):
-        best = 2**32-1
+        # Search bins until one that can accomodate the requirements
+        # while leaving minimum space is found.
+        
+        best = 4294967295  # any large number
         b = -1
         for i in range(self.BINS):
             if (req <= self.slack[i]) and (self.slack[i] < best):
@@ -52,6 +74,9 @@ class BinPack(object):
         return b, c
     
     def __find_worst_bin(self, req):
+        # Search bins until one that can accomodate the requirements
+        # while leaving maximum space is found.
+
         best = 0
         b = -1
         for i in range(self.BINS):
@@ -68,18 +93,17 @@ class BinPack(object):
 
 if __name__ == '__main__':
     term = [7, 2, 6, 5, 2, 6, 9, 3]
+    # term = [16, 4, 1]
     print(term)
 
     bins = BinPack(4, 20)
-    result1, fit1 = bins.fit(term)
+    # bins = BinPack(2, 16)
+    result1 = bins.fit(term)
     print(result1)
-    print(fit1)
 
-    result2, fit2 = bins.fit(term, type="BEST")
+    result2 = bins.fit(term, type="BEST")
     print(result2)
-    print(fit2)
-
-    result3, fit3 = bins.fit(term, type="WORST")
+    
+    result3 = bins.fit(term, type="WORST")
     print(result3)
-    print(fit3)
 
