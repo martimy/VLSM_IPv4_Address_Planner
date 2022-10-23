dd016aa2c1879189f8afe3287731b0120b867944
Maen Artimy <artimym@gmail.com>
2022-10-22 21:32:06 -0300

    modify examples

--- vlsm_subnet_calc.py
+++ vlsm_subnet_calc.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     network = '10.10.0.0/21'
     req_subnets = {'A': 177, 'B': 160, 'C': 50,
                    'D': 62, 'E': 123, 'F': 104, 'G': 67, 'H': 67}
-    plan = AddressPlan4(req_subnets)
+    plan = AddressPlan4(req_subnets, scale=1.1)
     #plan.plan_old(network, req_subnets)
     result = plan.plan(network)
     print(result.get_output())
@@ -39,7 +39,7 @@ if __name__ == '__main__':
                    'B3_Sales': 575, 'B3_DMZ': 5, 'B4_Sales': 75,
                    'B4_Market': 75, 'B4_DMZ': 5, 'B5_Sales': 80, 'P2P': 10}
     plan = AddressPlan4(req_subnets, scale=1.0)
-    result = plan.plan(network)
+    result = plan.plan(network, method="WORST")
     print(result.get_output())
     # print('Allocated = {:0.1f}'.format(alloc))
     print(plan.get_map())
