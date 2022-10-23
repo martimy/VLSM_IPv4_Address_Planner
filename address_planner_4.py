6f16a0211c9a38baf28d1c74174dac8653917d0a
Maen Artimy <artimym@gmail.com>
2022-10-22 21:31:42 -0300

    add algorithm description

--- address_planner_4.py
+++ address_planner_4.py
@@ -18,6 +18,40 @@
 An VLSM address planner for IPv4
 """
 
+"""
+Algorithm:
+
+1. Get the number of bits required for each subnet
+2. Get the mask extension needed for each requirements
+3. Get the number of subnets that result from 2
+4. Check if the requirements can be accommodated
+5. Calculate the size of blocks
+6. Use the maximum subnet size as unit to determine the Bin size and
+7. Use Bin Pack Algorithm to get block to bin assignment
+8. Assign network addresses
+
+
+# Example:
+
+Say you are give the prefix 192.168.4.0/24 and asked to subnet it to networks of 72, 15, and 6 hosts each (ignore growth).
+
+1. The required bits for each: 7, 5, 3
+2. 32-24-7 = 1, 32-24-5 = 3, 32-24-3 = 5
+3. num_subnets = 2, 8, 32
+4. Utilization = 1/2+1/8+1/32 = 0.65625
+5. blocks = [32/2, 32/8, 32/32] = [16, 4, 1]
+6. num_bins = min(num_subnets) = 2
+   bin_size = max(blocks) = 16
+7. fitted = [(0,0), (1,0), (1,4)]
+   result = [0, 16, 20]
+8. block size = 2^3 = 8
+   mult = [0, 128, 160]
+   ext_bits = [1, 3, 5]
+9. 192.168.4.0 + 0 / 25
+   192.168.4.0 + 128 / 27
+   192.168.4.0 + 160 / 29
+"""
+
 import math
 import ipaddress
 from binpackmod import BinPack
